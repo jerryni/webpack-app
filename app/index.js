f6e0614fcd16dc7039bb1d9ea2336e1850349571
nirizhe <nirizhe@youzan.com>
2018-07-22 12:41:21 +0800

    feat: js support hmr

--- app/index.js
+++ app/index.js
@@ -2,5 +2,13 @@ import './scss/main.scss'
 import run from './async.js'
 
 run().then(email => {
-    document.getElementById('app').innerHTML = `Email: ${email}`;
+    document.getElementById('app').innerHTML = `Email: ${email}`
 })
+
+if (module.hot) {
+    module.hot.accept('./async.js', function() {
+        run().then(email => {
+            document.getElementById('app').innerHTML = `Email: ${email}`
+        })
+    })
+}
